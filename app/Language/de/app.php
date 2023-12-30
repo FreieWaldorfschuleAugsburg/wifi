84c24320483661a6ae5e8b07952aa86da1ef6a77
elektr0nisch <contact@elektronisch.dev>
2023-12-30 23:34:35 +0100

    Happy new year

--- app/Language/de/app.php
+++ app/Language/de/app.php
@@ -6,7 +6,7 @@ return [
         'full' => 'WiFi-System Waldorf Augsburg',
         'headline' => 'WiFi-System<br/>Waldorf Augsburg'
     ],
-    'copyright' => '&copy; 2023 Freie Waldorfschule und Waldorfkindergärten Augsburg e. V.<br><small>Entwickelt von <a href="https://elektronisch.dev" target="_blank">Linus Groschke</a> <br/> <a href="https://github.com/FreieWaldorfschuleAugsburg/wifi" target="_blank">Quellcode auf GitHub</a> – <a href="https://waldorf-augsburg.de/impressum" target="_blank">Impressum</a></small></small>',
+    'copyright' => '&copy; 2024 Freie Waldorfschule und Waldorfkindergärten Augsburg e. V.<br><small>Entwickelt von <a href="https://elektronisch.dev" target="_blank">Linus Groschke</a> <br/> <a href="https://github.com/FreieWaldorfschuleAugsburg/wifi" target="_blank">Quellcode auf GitHub</a> – <a href="https://waldorf-augsburg.de/impressum" target="_blank">Impressum</a></small></small>',
     'error' => [
         'exception' => [
             'title' => 'Systemfehler!',
--- app/Language/en/app.php
+++ app/Language/en/app.php
@@ -6,7 +6,7 @@ return [
         'full' => 'WiFi system of the Freie Waldorfschule Augsburg',
         'headline' => 'WiFi system of the<br/>Freie Waldorfschule Augsburg'
     ],
-    'copyright' => '&copy; 2023 Freie Waldorfschule Augsburg<br><small>Developed by <a href="https://elektronisch.dev" target="_blank">Linus Groschke</a> <br/> <a href="https://github.com/FreieWaldorfschuleAugsburg/wifi" target="_blank">Source on GitHub</a> – <a href="https://waldorf-augsburg.de/impressum" target="_blank">Legal notice</a></small></small>',
+    'copyright' => '&copy; 2024 Freie Waldorfschule und Waldorfkindergärten Augsburg e. V.<br><small>Developed by <a href="https://elektronisch.dev" target="_blank">Linus Groschke</a> <br/> <a href="https://github.com/FreieWaldorfschuleAugsburg/wifi" target="_blank">Source on GitHub</a> – <a href="https://waldorf-augsburg.de/impressum" target="_blank">Legal notice</a></small></small>',
     'error' => [
         'exception' => [
             'title' => 'Error!',
